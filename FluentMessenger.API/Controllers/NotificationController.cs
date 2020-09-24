f83e218f73435e3555f022e003deb407dc4359c7
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-09-24 02:47:44 +0100

    commit change in Notification Controller

--- FluentMessenger.API/Controllers/NotificationController.cs
+++ FluentMessenger.API/Controllers/NotificationController.cs
@@ -6,10 +6,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace FluentMessenger.API.Controllers {
     [Authorize]
